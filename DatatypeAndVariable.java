d9c03163f28d7646163c0dfaf2f0e767f54cd0fb
789sandeep <sandeepprajapti789@gmail.com>
2024-11-07 15:34:54 +0530

    try

---
+++ DatatypeAndVariable.java
@@ -0,0 +1,19 @@
+
+public class DatatypeAndVariable {
+    public static void main(String[] args) {
+        int a=23;
+        float f=34.000034400000f;
+        double d=4423.223;
+        char ch='q';
+        boolean bol=false;
+        String st="sandeep";
+       
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(bol);
+        System.out.println(st);
+    }
+   
+}
